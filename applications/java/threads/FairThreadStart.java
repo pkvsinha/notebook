897c0cc0f8dfe83497f3da670c6e6b073bfeae11
Prashant Sinha <pkv.sinha492@gmail.com>
2018-03-08 00:04:54 +0530

    java

---
+++ applications/java/threads/FairThreadStart.java
@@ -0,0 +1,9 @@
+/***
+    This class shows how to start n threads, but they should start runnign at the same time.
+ */
+public class FairThreadStart {
+
+    public static long start( int threads, final Runnable task ) {
+        
+    }
+}
---
+++ applications/java/threads/SingleThreadAccess.java
@@ -0,0 +1,85 @@
+public class SingleThreadAccess {
+    
+    private Object lock = new Object();
+    
+    private Object value;
+
+    public Object take() {
+        boolean shouldWait = false;
+        synchronized(this) {
+            if( this.value != null ) {
+                shouldWait = true;
+            }
+        }
+        if ( shouldWait ) {
+            try{
+                lock.wait();
+            } catch(InterruptedException e) {
+                System.err.println("Consumer is broken");
+            }
+            
+        } 
+        Object temp = null;
+        synchronized( this ) {
+            temp = this.value;
+            this.value = null;
+        }
+        lock.notifyAll();
+        return temp;
+    }
+
+    public void put( Object value ) {   
+        boolean shouldWait = false;
+        synchronized(this) {
+            if( this.value != null ) {
+                shouldWait = true;
+            }
+        }
+        if ( shouldWait ) {
+            try{
+                lock.wait();
+            } catch(InterruptedException e) {
+                System.err.println("Producer is broken");
+            }
+        }  
+        synchronized(this) {
+            this.value = value;
+        }   
+        
+        lock.notifyAll();       
+    }
+
+    public static void main(String[] args) {
+        final SingleThreadAccess queue = new SingleThreadAccess();
+        Thread producer = new Thread( new Runnable() {
+            public void run() {
+                while(true) {
+                    long timeTowait = Math.round( Math.ceil( Math.random() * 5000 ) );
+                    try {
+                        System.out.println("Producer is sleeping for " + timeTowait);
+                        Thread.sleep(timeTowait);
+                    } catch(InterruptedException e) {
+                        throw new RuntimeException();
+                    } 
+                    queue.put(timeTowait);
+                }                
+            }
+        });
+        
+        Thread consumer = new Thread( new Runnable() {
+            public void run() {
+                while(true) {
+                    long timeTowait = Math.round( Math.ceil( Math.random() * 5000 ) );
+                    try{
+                        Thread.sleep(timeTowait);
+                    } catch(InterruptedException e) {
+                        throw new RuntimeException();
+                    }                    
+                    System.out.println("Producer was sleeping for " + queue.take() );
+                }                
+            }
+        });
+        producer.start();
+        consumer.start();
+    }
+}
